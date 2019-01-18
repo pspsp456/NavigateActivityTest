65833700b96f88bf1e0703d4e8ac0200344f9a4a
pspsp456 <pspsp456@gmail.com>
2019-01-18 18:08:10 +0900

    profile 건드록 있음 tab페이지 공부 및 구현 중........

--- app/src/main/java/com/example/navigateactivitytest/ProfileActivity.java
+++ app/src/main/java/com/example/navigateactivitytest/ProfileActivity.java
@@ -1,27 +1,43 @@
 package com.example.navigateactivitytest;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.SectionIndexer;
 import android.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private  static final  String TAG = "ProfileActivity";
+
+    private SectionPageAdapter mSectionAdapter;
+
+    private  ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mSectionAdapter = new SectionPageAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager)findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN)
-        {
-        }
-        return super.onTouchEvent(event);
+    private void setupViewPager(ViewPager viewPager)
+    {
+        SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new tab1Activity(), "TAB1");
+
+        viewPager.setAdapter(adapter);
     }
 
 }
---
+++ app/src/main/java/com/example/navigateactivitytest/tab1Activity.java
@@ -0,0 +1,25 @@
+package com.example.navigateactivitytest;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class tab1Activity extends Fragment {
+    private  static final  String TAG = "Tab1Fregment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+
+        return view;
+    }
+}
